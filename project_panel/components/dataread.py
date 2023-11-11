880903adcfa903d135b1c60c30f5fb732e42ff9d
hamed46 <47857182+hamed46@users.noreply.github.com>
2023-11-11 11:02:08 +0100

    Delete project_panel directory

--- project_panel/components/dataread.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-def read_csv(path):
-    df = pd.read_csv(path)
-    df.drop(['Unnamed: 0', 'track_id'], axis=1, inplace=True)
-    df.drop_duplicates(subset=['track_name'], keep='first', inplace=True)
-    return df
